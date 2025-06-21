02e56c5e974cd158e2948e7ea97a0029d75e4e6f
asmahussain48 <asma.hussain.mughal@gmail.com>
2025-06-21 21:02:35 +0500

    Updated Commit

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,27 @@
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import javax.swing.*;
 public class Calculator{
 
         int boarderWidth = 360;
         int boarderHeight = 540;
-       
-        Color customLightGrey = new Color(212,212, 210);
-        Color customDarkGrey = new Color(80,80,80); 
-        Color customBlack = new Color(28,28,28);
-        Color customOrange = new Color(255,149,0);
+
+        String[] buttonValues = {       
+                "AC", "+/-", "%", "/",
+                "7", "8", "9", "x",
+                "4", "5", "6", "-",
+                "1", "2", "3", "+",
+                "0", ".","√", "="
+        };
+        String[] rightSymbols = {"/", "x", "-", "+", "="};
+        String[] topSymbols = {"AC", "+/-", "%"};
         
         JFrame  frame = new JFrame();
         JLabel displayLabel = new JLabel();
         JPanel displayPanel = new JPanel();
+        JPanel buttonsPanel = new JPanel();
 
 
 
@@ -25,7 +34,53 @@ public class Calculator{
                 frame.setResizable(false);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setLayout(new BorderLayout()); // Set to north, south , west, east
-                displayLabel.setBackground(customBlack);
-                displayLabel.setForeground(Color.white);                
+
+                displayLabel.setBackground(Color.BLACK);
+                displayLabel.setForeground(Color.WHITE);  
+                displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+                displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+                displayLabel.setText("0"); // defalut text 
+                displayLabel.setOpaque(true);
+
+                displayPanel.setLayout(new BorderLayout());
+                displayPanel.add(displayLabel);
+                frame.add(displayPanel, BorderLayout.NORTH);
+
+                buttonsPanel.setLayout(new GridLayout(5, 4)); // 5 rows and 4 columns
+                buttonsPanel.setBackground(Color.BLACK);
+                // Add a border to the buttons panel
+                // Border border = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+                // buttonsPanel.setBorder(border);
+                frame.add(buttonsPanel);
+                
+                for(int i = 0; i<buttonValues.length; i++){
+                        JButton button = new JButton();
+                        String buttonValue = buttonValues[i];
+                        button.setFont(new Font("Arial", Font.PLAIN, 30));
+                        button.setText(buttonValue);
+                        button.setFocusable(false);// to remove the recatngle our text
+                        if(Arrays.asList(topSymbols).contains(buttonValue)){
+                                button.setBackground(Color.lightGray);
+                                button.setForeground(Color.BLACK);
+
+                        } else if(Arrays.asList(rightSymbols).contains(buttonValue)){
+                                button.setBackground(new Color(255,149,0));
+                                button.setForeground(Color.WHITE);
+                        } else {
+                                button.setBackground(Color.DARK_GRAY);
+                                button.setForeground(Color.WHITE);
+                        }
+
+                        buttonsPanel.add(button);
+
+                }
+
+
+        }
+
+
+          public static void main(String[] args) {
+        // Obje ct to call the calculator
+                Calculator calculator = new Calculator();
         }
 }
--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,8 +1,5 @@
 public class CalculatorApp {
-     public static void main(String[] args) {
-        // Obje ct to call the calculator
-                Calculator calculator = new Calculator();
-        }
+   
 
     
 }
