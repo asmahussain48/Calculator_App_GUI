4eb7bf0d772e0ca8e86c2d4689fbd0ed9ea70e42
asmahussain48 <asma.hussain.mughal@gmail.com>
2025-06-21 17:29:22 +0500

    Updated Commit

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,5 +1,6 @@
 public class CalculatorApp {
      public static void main(String[] args) {
+        // Obje ct to call the calculator
                 Calculator calculator = new Calculator();
         }
     
--- demo.py
+++
@@ -1,2 +0,0 @@
-print("Hellow World")
-print("Python first file")
