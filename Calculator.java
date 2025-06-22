3c06f959f33a3ac76d066cbfa9461f17f1bb56ee
asmahussain48 <asma.hussain.mughal@gmail.com>
2025-06-22 20:20:30 +0500

    Updated

--- Calculator.java
+++ Calculator.java
@@ -34,112 +34,146 @@ public class Calculator{
         JPanel displayPanel = new JPanel();
         JPanel buttonsPanel = new JPanel();
 
-
-
-
         Calculator(){
-                frame.setTitle("Calculator");
-                frame.setVisible(true);
-                frame.setSize(boarderWidth, boarderHeight);
-                frame.setLocationRelativeTo(null); // This centers the Window Frame
-                frame.setResizable(false);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout()); // Set to north, south , west, east
 
-                displayLabel.setBackground(Color.BLACK);
-                displayLabel.setForeground(Color.WHITE);  
-                displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
-                displayLabel.setHorizontalAlignment(JLabel.RIGHT);
-                displayLabel.setText("0"); // defalut text 
-                displayLabel.setOpaque(true);
+        frame.setTitle("Calculator");
+        // frame.setVisible(true); // make it in end for any issue 
+        frame.setSize(boarderWidth, boarderHeight);
+        frame.setLocationRelativeTo(null); // This centers the Window Frame
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout()); // Set to north, south , west, east
 
-                displayPanel.setLayout(new BorderLayout());
-                displayPanel.add(displayLabel);
-                frame.add(displayPanel, BorderLayout.NORTH);
+        displayLabel.setBackground(Color.BLACK);
+        displayLabel.setForeground(Color.WHITE);  
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0"); // defalut text 
+        displayLabel.setOpaque(true);
 
-                buttonsPanel.setLayout(new GridLayout(5, 4)); // 5 rows and 4 columns
-                buttonsPanel.setBackground(Color.BLACK);
-                // Add a border to the buttons panel
-                // Border border = BorderFactory.createEmptyBorder(10, 10, 10, 10);
-                // buttonsPanel.setBorder(border);
-                frame.add(buttonsPanel);
-                
-                for(int i = 0; i<buttonValues.length; i++){
-                        JButton button = new JButton();
-                        String buttonValue = buttonValues[i];
-                        button.setFont(new Font("Arial", Font.PLAIN, 30));
-                        button.setText(buttonValue);
-                        button.setFocusable(false);// to remove the recatngle our text
-                        button.setBorder(new LineBorder(Color.black,1));
-                        // Prevents the button from showing a focus border when selected or clicked
-                        button.setFocusPainted(false); 
-                        if(Arrays.asList(topSymbols).contains(buttonValue)){
-                                button.setBackground(Color.lightGray);
-                                button.setForeground(Color.BLACK);
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel, BorderLayout.NORTH);
 
-                        } else if(Arrays.asList(rightSymbols).contains(buttonValue)){
-                                button.setBackground(new Color(255,149,0));
-                                button.setForeground(Color.WHITE);
-                        } else {
-                                button.setBackground(Color.DARK_GRAY);
-                                button.setForeground(Color.WHITE);
-                        }
+        buttonsPanel.setLayout(new GridLayout(5, 4)); // 5 rows and 4 columns
+        buttonsPanel.setBackground(Color.BLACK);
+        // Add a border to the buttons panel
+        // Border border = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+        // buttonsPanel.setBorder(border);
+        frame.add(buttonsPanel);
+        
+        for(int i = 0; i<buttonValues.length; i++){
+                JButton button = new JButton();
+                String buttonValue = buttonValues[i];
+                button.setFont(new Font("Arial", Font.PLAIN, 30));
+                button.setText(buttonValue);
+                button.setFocusable(false);// to remove the recatngle our text
+                button.setBorder(new LineBorder(Color.black,1));
+                // Prevents the button from showing a focus border when selected or clicked
+                button.setFocusPainted(false); 
+                if(Arrays.asList(topSymbols).contains(buttonValue)){
+                        button.setBackground(Color.lightGray);
+                        button.setForeground(Color.BLACK);
 
-                        buttonsPanel.add(button);
-                        button.addActionListener(new ActionListener(){
-                                public void actionPerformed(ActionEvent e){
-                                        JButton button = (JButton) e.getSource();
-                                        String buttonValue = button.getText();
-                                        if(Arrays.asList(rightSymbols).contains(buttonValue)){
-
-                                        } else if(Arrays.asList(topSymbols).contains(buttonValue)){
-                                                //conditions for top Symbols 
-                                                if(buttonValue == "AC"){
-                                                        // function to clear A, Operator, B
-                                                        clearAll();
-                                                        displayLabel.setText("0"); // setting back to 0 
-                                                }
-                                                else if(buttonValue == "+/-"){
-                                                        double numDisplay = Double.parseDouble(displayLabel.getText());
-                                                        numDisplay = numDisplay*(-1);
-                                                        //removeZeroDecimal is not built in method
-                                                         displayLabel.setText(removeZeroDecimal(numDisplay)); 
-                                                        
-
-                                                }
-                                                else if(buttonValue == "%"){
-                                                         double numDisplay = Double.parseDouble(displayLabel.getText());
-                                                        numDisplay = numDisplay/(100);
-                                                        //removeZeroDecimal is not built in method
-                                                         displayLabel.setText(removeZeroDecimal(numDisplay)); 
-                                                }
-                                        } else {
-                                                 if(buttonValue == "."){
-                                                        // if a number doesn't have decimal . then we can add one . but if it has it can not have . anothe
-                                                        // =2.45 is okay but 2.45. is not this below code is for handling this
-                                                        if(!displayLabel.getText().contains(buttonValue)){
-                                                                displayLabel.setText(displayLabel.getText() + buttonValue);
-                                                                
-                                                        }
-                                                 }
-                                                 else if("0123456789".contains(buttonValue)){
-                                                        // 05 = 5
-                                                        if(displayLabel.getText() == "0"){
-                                                                displayLabel.setText(buttonValue);
-                                                        }
-                                                        else{
-                                                                displayLabel.setText(displayLabel.getText() + buttonValue);
-
-                                                        }
-                                                 }
-                                        }
-                                }
-                        });
-
-
+                } else if(Arrays.asList(rightSymbols).contains(buttonValue)){
+                        button.setBackground(new Color(255,149,0));
+                        button.setForeground(Color.WHITE);
+                } else {
+                        button.setBackground(Color.DARK_GRAY);
+                        button.setForeground(Color.WHITE);
                 }
 
+                buttonsPanel.add(button);
 
+                button.addActionListener(new ActionListener(){
+
+                public void actionPerformed(ActionEvent e){
+                JButton button = (JButton) e.getSource();
+                String buttonValue = button.getText();
+                        if(Arrays.asList(rightSymbols).contains(buttonValue)){
+                                
+                        if (buttonValue == "="){
+                                if(A != null){
+                                B = displayLabel.getText();
+                                double numA = Double.parseDouble(A);
+                                double numB = Double.parseDouble(B);
+                                
+                                if (Operator == "+"){
+                                        displayLabel.setText(removeZeroDecimal(numA + numB)); // getting String Value
+                                }
+                                else if (Operator == "-"){
+                                        displayLabel.setText(removeZeroDecimal(numA - numB)); // getting String Value
+                                }
+                                else if (Operator == "x"){
+                                        displayLabel.setText(removeZeroDecimal(numA * numB)); // getting String Value
+                                }
+                                else if (Operator == "/"){
+                                        displayLabel.setText(removeZeroDecimal(numA / numB)); // getting String Value
+                                }
+                                }
+                        }
+                        else if ("+-x/".contains(buttonValue)){
+                                if(Operator == null){
+                                        A = displayLabel.getText(); // setting A to the any value that has written in displayLabel
+                                        displayLabel.setText("0");
+                                        B = "0";
+                                }
+                                // if user type button twise etc
+                                Operator = buttonValue;
+
+                        }
+
+
+
+                        } 
+                        else if (Arrays.asList(topSymbols).contains(buttonValue)){
+                        //conditions for top Symbols 
+                        if(buttonValue == "AC"){
+                                // function to clear A, Operator, B
+                                clearAll();
+                                displayLabel.setText("0"); // setting back to 0 
+                        }
+                        else if(buttonValue == "+/-"){
+                                double numDisplay = Double.parseDouble(displayLabel.getText());
+                                numDisplay = numDisplay*(-1);
+                                //removeZeroDecimal is not built in method
+                                        displayLabel.setText(removeZeroDecimal(numDisplay)); 
+                                
+
+                        }
+                        else if(buttonValue == "%"){
+                                        double numDisplay = Double.parseDouble(displayLabel.getText());
+                                numDisplay = numDisplay/(100);
+                                //removeZeroDecimal is not built in method
+                                        displayLabel.setText(removeZeroDecimal(numDisplay)); 
+                        }
+                } else {
+                                if(buttonValue == "."){
+                                // if a number doesn't have decimal . then we can add one . but if it has it can not have . anothe
+                                // =2.45 is okay but 2.45. is not this below code is for handling this
+                                if(!displayLabel.getText().contains(buttonValue)){
+                                        displayLabel.setText(displayLabel.getText() + buttonValue);
+                                        
+                                }
+                                }
+                                else if("0123456789".contains(buttonValue)){
+                                // 05 = 5
+                                if(displayLabel.getText() == "0"){
+                                        displayLabel.setText(buttonValue);
+                                }
+                                else{
+                                        displayLabel.setText(displayLabel.getText() + buttonValue);
+
+                                }
+                                }
+                }
+        }
+                });
+
+
+        }
+
+        frame.setVisible(true); 
         }
 
         void clearAll(){
